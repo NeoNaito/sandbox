fb32b4f1fe837d7402dd281ecff38d33fd33669b
NeoNaito <neo.akihiro.naito@gmail.com>
2025-08-25 18:36:21 +0900

    Update cose-algorithms-adding-sample.java

--- cose-algorithms-adding-sample.java
+++ cose-algorithms-adding-sample.java
@@ -1,7 +1,8 @@
 List<PublicKeyCredentialParameters> pubKeyCredParams = new ArrayList<>();
 String algorithms = "-7,-257,-8";
+String type = "public-key";
 for (String alg : algorithms.split(",")) {
     int algInt = Integer.parseInt(alg);
     COSEAlgorithmIdentifier coseAlg = COSEAlgorithmIdentifier.create(algInt);
-    pubKeyCredParams.add(new PublicKeyCredentialParameters(PublicKeyCredentialType.PUBLIC_KEY, coseAlg));
+    pubKeyCredParams.add(new PublicKeyCredentialParameters(PublicKeyCredentialType.create(type), coseAlg));
 }
